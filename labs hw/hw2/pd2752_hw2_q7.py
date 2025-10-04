f0de5fb7f561cbcf22014e653124a4d5ddee7be7
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-10-03 22:23:47 -0400

    q7 answered for hw2

--- labs hw/hw2/pd2752_hw2_q7.py
+++ labs hw/hw2/pd2752_hw2_q7.py
@@ -1,1 +1,17 @@
 #q7 answer pd2752
+def findChange(lst01):
+    left = 0
+    right = len(lst01) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if lst01[mid] == 0:
+            left = mid + 1
+        else:
+            right = mid
+    if lst01[left] == 1:
+        return left
+    else:
+        return float('-inf')
+
+# print(findChange([0,0,0,1,1,1])) 
+# print(findChange([0,0,0,0]))      
