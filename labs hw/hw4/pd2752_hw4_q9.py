7878296c72b29c4c8546b3add5610b82d354f08c
pragun damani <damanipragun@proton.me>
2025-10-24 11:32:04 -0400

    lab 8

--- labs hw/hw4/pd2752_hw4_q9.py
+++ labs hw/hw4/pd2752_hw4_q9.py
@@ -1,1 +1,16 @@
-#q9 answer pd2752
+def permutations(lst,low,high):
+    sub = lst[low:high+1]
+    if len(sub)==0:
+        return []
+    if len(sub)==1:
+        return [[sub[0]]]
+    out = []
+    for i in range(len(sub)):
+        elem = sub[i]
+        rem = sub[:i]+ sub[i+1:]
+        for p in permutations(rem,0,len(rem)-1):
+            out.append([elem]+p)
+    return out
+
+# lst = [1,2,3]
+# print(permutations(lst,0,2))
