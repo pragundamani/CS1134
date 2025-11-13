319a9f70a19b715214d0fcc9bdd6cbfa53ce1c3e
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-13 22:53:56 +0000

    Implement LinkedBinaryTree class with traversal and utility methods

--- example.py
+++ example.py
@@ -1,3 +1,13 @@
-from ArrayQueue import ArrayQueue
+from LinkedBinaryTree import LinkedBinaryTree
 
+def tree_sum(tree):
+    total = 0
+    def child_sum(tree,the_node):
+        total += the_node.data
+        if the_node.left != None:
+            child_sum(tree,the_node.left)
+        if the_node.right != None:
+            child_sum(tree,the_node.right)
+    child_sum(tree,tree.root)
 
+    return total
--- rec/scratch.py
+++ rec/scratch.py
@@ -2,4 +2,4 @@ from doublelylinkedlist import DoublyLinkedList
 
 def linked_list_mover(s,num):
     header = 0
-    t
+    
