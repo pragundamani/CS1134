ffd659b39d9216a14ef6d6c2406be60254fc1c97
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-14 21:41:20 +0000

    Refactor code structure for improved readability and maintainability

--- example.py
+++ example.py
@@ -1,1 +1,18 @@
-def check(root):
+from DoublyLinkedList import DoublyLinkedList
+def move_to_last(self,node):
+    node.prev.next = node.next
+    node.next.prev = node.prev
+    self.trailer.prev.next = node
+    node.prev = self.trailer.prev
+    self.trailer.prev = node
+    node.next = self.trailer
+    
+from ArrayStack import ArrayStack
+from ArrayQueue import ArrayQueue
+
+def alternating_parity(lst):
+    def is_even(n):
+        return n % 2 == 0
+    def consisten(a,b):
+        return (is_even(a) and not is_even(b)) or not (is_even(a) and not is_even(b))
+    
