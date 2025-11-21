92a719d257d38324a68483c803dd412a38c8a811
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-21 13:29:24 -0500

    backup

--- example.py
+++ example.py
@@ -1,18 +1,15 @@
-from DoublyLinkedList import DoublyLinkedList
-def move_to_last(self,node):
-    node.prev.next = node.next
-    node.next.prev = node.prev
-    self.trailer.prev.next = node
-    node.prev = self.trailer.prev
-    self.trailer.prev = node
-    node.next = self.trailer
-    
-from ArrayStack import ArrayStack
-from ArrayQueue import ArrayQueue
-
-def alternating_parity(lst):
-    def is_even(n):
-        return n % 2 == 0
-    def consisten(a,b):
-        return (is_even(a) and not is_even(b)) or not (is_even(a) and not is_even(b))
-    
+def find(root,key):
+    if root is None:
+        return ['fail']
+    elif root.value == key:
+        return []
+    lst = [
+    val = root.val
+    if key>val:
+        find(root.right,key)
+        return ['r']
+    if key<val:
+        find(root.left,key)
+        return ['l']
+    return      
+        
