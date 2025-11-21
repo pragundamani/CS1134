8efc11f972224c241e8bbf001445836ae238935f
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-21 15:27:34 -0500

    hw6

--- labs hw/hw6/pd2752_hw6_q1.py
+++ labs hw/hw6/pd2752_hw6_q1.py
@@ -1,1 +1,25 @@
 #q1 answer pd2752
+from DoublyLinkedList import DoublyLinkedList
+
+class LinkedQueue:
+    def __init__(self):
+        self.data = DoublyLinkedList()
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def is_empty(self):
+        return self.data.is_empty()
+    
+    def enqueue(self,e):
+        self.data.add_last(e)
+        
+    def dequeue(self):
+        if self.is_empty():
+            raise Exception("Queue is empty")
+        return self.data.delete_first()
+    
+    def first(self):
+        if self.is_empty():
+            raise Exception("Queue is empty")
+        return self.data.header.next.data
--- labs hw/hw6/pd2752_hw6_q2.py
+++ labs hw/hw6/pd2752_hw6_q2.py
@@ -1,1 +1,31 @@
 #q2 answer pd2752
+from DoublyLinkedList import DoublyLinkedList
+
+class Integer:
+    def __init__(self,num_str):
+        self.data = DoublyLinkedList()
+        for ch in num_str:
+            self.data.add_last(int(ch))
+    
+    def __add__(self,other):
+        carry = 0
+        result = Integer("")
+        node1 = self.data.trailer.prev
+        node2 = other.data.trailer.prev
+        while node1 is not self.data.header or node2 is not other.data.header:
+            val1 = node1.data if node1 is not self.data.header else 0
+            val2 = node2.data if node2 is not other.data.header else 0
+            total = val1+val2+carry
+            result.data.add_first(total%10)
+            carry = total//10
+            node1 = node1.prev if node1 is not self.data.header else None
+            node2 = node2.prev if node2 is not other.data.header else None
+        if carry>0:
+            result.data.add_first(carry)
+        return result
+
+    def __repr__(self):
+        return "".join(str(digit) for digit in self.data)
+    
+    def __mul__(self,other):
+        pass
