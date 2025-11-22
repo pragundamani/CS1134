8c10f8f240c157efa9af1025d2a96b3781412a9f
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-21 21:46:08 -0500

    Update pd2752_hw6_q4.py

--- labs hw/hw6/pd2752_hw6_q4.py
+++ labs hw/hw6/pd2752_hw6_q4.py
@@ -1,1 +1,25 @@
 #q4 answer pd2752
+from DoublyLinkedList import DoublyLinkedList
+
+def copy_linked_list(lnk_lst):
+    new_list = DoublyLinkedList()
+    cursor = lnk_lst.header.next
+    
+    while cursor is not lnk_lst.trailer:
+        new_list.add_last(cursor.data)
+        cursor = cursor.next
+    
+    return new_list
+
+def deep_copy_linked_list(lnk_lst):
+    new_list = DoublyLinkedList()
+    cursor = lnk_lst.header.next
+    
+    while cursor is not lnk_lst.trailer:
+        if isinstance(cursor.data, DoublyLinkedList):
+            new_list.add_last(deep_copy_linked_list(cursor.data))
+        else:
+            new_list.add_last(cursor.data)
+        cursor = cursor.next
+    
+    return new_list
