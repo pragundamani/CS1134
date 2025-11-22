e093358589259cebb9085140a7ce96bd1de22578
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-21 21:56:52 -0500

    Refactor copy and deep copy functions for DLL

--- labs hw/hw6/pd2752_hw6_q4.py
+++ labs hw/hw6/pd2752_hw6_q4.py
@@ -1,25 +1,21 @@
 #q4 answer pd2752
 from DoublyLinkedList import DoublyLinkedList
 
-def copy_linked_list(lnk_lst):
-    new_list = DoublyLinkedList()
-    cursor = lnk_lst.header.next
+def copy_linked_list(dll):
+    res = DoublyLinkedList()
+    curr = dll.header.next
+    while curr is not dll.trailer:
+        res.add_last(curr.data)
+        curr = curr.next
+    return res
     
-    while cursor is not lnk_lst.trailer:
-        new_list.add_last(cursor.data)
-        cursor = cursor.next
-    
-    return new_list
-
-def deep_copy_linked_list(lnk_lst):
-    new_list = DoublyLinkedList()
-    cursor = lnk_lst.header.next
-    
-    while cursor is not lnk_lst.trailer:
-        if isinstance(cursor.data, DoublyLinkedList):
-            new_list.add_last(deep_copy_linked_list(cursor.data))
+def deep_copy_linked_list(dll):
+    res = DoublyLinkedList()
+    curr = dll.header.next
+    while curr is not dll.trailer:
+        if isinstance(curr.data,DoublyLinkedList):
+            res.add_last(deep_copy_linked_list(curr.data))
         else:
-            new_list.add_last(cursor.data)
-        cursor = cursor.next
-    
-    return new_list
+            res.add_last(curr.data)
+        curr = curr.next
+    return res
