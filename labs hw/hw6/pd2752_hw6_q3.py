38d12f68d4d83cec62c391e92d36057741cbc8ae
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-21 21:55:22 -0500

    Refactor CompactString class variable names

--- labs hw/hw6/pd2752_hw6_q3.py
+++ labs hw/hw6/pd2752_hw6_q3.py
@@ -2,78 +2,78 @@
 from DoublyLinkedList import DoublyLinkedList
 
 class CompactString:
-    def __init__(self, orig_str):
+    def __init__(self, og_str):
         self.data = DoublyLinkedList()
-        if not orig_str:
+        if not og_str:
             return
         
-        i = 0
-        while i < len(orig_str):
-            char = orig_str[i]
+        n = 0
+        while n < len(og_str):
+            char = og_str[n]
             count = 1
-            while i + count < len(orig_str) and orig_str[i + count] == char:
+            while n+count < len(og_str) and og_str[n+count] == char:
                 count += 1
             self.data.add_last((char, count))
-            i += count
+            n += count
     
     def __add__(self, other):
-        result = CompactString('')
+        res = CompactString('')
         
         for char, count in self.data:
-            result.data.add_last((char, count))
+            res.data.add_last((char,count))
         
         if not self.data.is_empty() and not other.data.is_empty():
-            last_self = self.data.trailer.prev.data
-            first_other = other.data.header.next.data
+            self_end = self.data.trailer.prev.data
+            oth_start = other.data.header.next.data
             
-            if last_self[0] == first_other[0]:
-                result.data.trailer.prev.data = (last_self[0], last_self[1] + first_other[1])
-                cursor = other.data.header.next.next
+            if self_end[0] == oth_start[0]:
+                res.data.trailer.prev.data = (self_end[0],self_end[1]+oth_start[1])
+                curr = other.data.header.next.next
             else:
-                cursor = other.data.header.next
+                curr = other.data.header.next
         else:
-            cursor = other.data.header.next
+            curr = other.data.header.next
         
-        while cursor is not other.data.trailer:
-            result.data.add_last(cursor.data)
-            cursor = cursor.next
+        while curr is not other.data.trailer:
+            res.data.add_last(curr.data)
+            curr = curr.next
         
-        return result
+        return res
     
     def __lt__(self, other):
-        cursor1 = self.data.header.next
-        cursor2 = other.data.header.next
-        count1_remaining = 0
-        count2_remaining = 0
+        curr1 = self.data.header.next
+        curr2 = other.data.header.next
+        counter1 = 0
+        counter2 = 0
         
-        while cursor1 is not self.data.trailer or cursor2 is not other.data.trailer or count1_remaining > 0 or count2_remaining > 0:
-            if count1_remaining == 0:
-                if cursor1 is not self.data.trailer:
-                    char1, count1_remaining = cursor1.data
-                    cursor1 = cursor1.next
+        while curr1 is not self.data.trailer or curr2 is not other.data.trailer or counter1 > 0 or counter2 > 0:
+            if counter1 == 0:
+                if curr1 is not self.data.trailer:
+                    char1, counter1 = curr1.data
+                    curr1 = curr1.next
                 else:
-                    return cursor2 is not other.data.trailer or count2_remaining > 0
+                    return curr2 is not other.data.trailer or counter2 > 0
             
-            if count2_remaining == 0:
-                if cursor2 is not other.data.trailer:
-                    char2, count2_remaining = cursor2.data
-                    cursor2 = cursor2.next
+            if counter2 == 0:
+                if curr2 is not other.data.trailer:
+                    char2, counter2 = curr2.data, curr2.data
+                    curr2 = curr2.next
                 else:
                     return False
             
-            if char1 < char2:
+            if char1<char2:
                 return True
-            elif char1 > char2:
+            elif char1>char2:
                 return False
             else:
-                min_count = min(count1_remaining, count2_remaining)
-                count1_remaining -= min_count
-                count2_remaining -= min_count
+                min_count = min(counter1,counter2)
+                counter1 -= min_count
+                counter2 -= min_count
         
         return False
     
     def __le__(self, other):
-        return self < other or self.__eq__(other)
+        return self<other or self.__eq__(other)
     
     def __gt__(self, other):
         return not self <= other
@@ -82,22 +82,22 @@ class CompactString:
         return not self < other
     
     def __repr__(self):
-        result = ''
+        res = ''
         for char, count in self.data:
-            result += char * count
-        return result
+            res += char*count
+        return res
     
-    def __eq__(self, other):
+    def __eq__(self,other):
         if len(self.data) != len(other.data):
             return False
         
-        cursor1 = self.data.header.next
-        cursor2 = other.data.header.next
+        curr1 = self.data.header.next
+        curr2 = other.data.header.next
         
-        while cursor1 is not self.data.trailer:
-            if cursor1.data != cursor2.data:
+        while curr1 is not self.data.trailer:
+            if curr1.data != curr2.data:
                 return False
-            cursor1 = cursor1.next
-            cursor2 = cursor2.next
+            curr1 = curr1.next
+            curr2 = curr2.next
         
         return True
