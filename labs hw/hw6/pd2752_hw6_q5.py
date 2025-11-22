d4734f6163b91ab00e81018fceecadb953825cc2
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-21 22:00:53 -0500

    Update pd2752_hw6_q5.py

--- labs hw/hw6/pd2752_hw6_q5.py
+++ labs hw/hw6/pd2752_hw6_q5.py
@@ -1,25 +1,25 @@
 #q5 answer pd2752
 from DoublyLinkedList import DoublyLinkedList
 
-def merge_linked_lists(srt_lnk_lst1, srt_lnk_lst2):
-    def merge_sublists(node1, node2, result_list):
-        if node1 is srt_lnk_lst1.trailer and node2 is srt_lnk_lst2.trailer:
+def merge_linked_lists(dll1, dll2):
+    def merge_sublists(noda_1, noda_2, res_lst):
+        if noda_1 is dll1.trailer and noda_2 is dll2.trailer:
             return
-        
-        if node1 is srt_lnk_lst1.trailer:
-            result_list.add_last(node2.data)
-            merge_sublists(node1, node2.next, result_list)
-        elif node2 is srt_lnk_lst2.trailer:
-            result_list.add_last(node1.data)
-            merge_sublists(node1.next, node2, result_list)
+            
+        if noda_1 is dll1.trailer:
+            res_lst.add_last(noda_2.data)
+            merge_sublists(noda_1, noda_2.next, res_lst)
+        elif noda_2 is dll2.trailer:
+            res_lst.add_last(noda_1.data)
+            merge_sublists(noda_1.next, noda_2, res_lst)
         else:
-            if node1.data <= node2.data:
-                result_list.add_last(node1.data)
-                merge_sublists(node1.next, node2, result_list)
+            if noda_1.data <= noda_2.data:
+                res_lst.add_last(noda_1.data)
+                merge_sublists(noda_1.next, noda_2, res_lst)
             else:
-                result_list.add_last(node2.data)
-                merge_sublists(node1, node2.next, result_list)
+                res_lst.add_last(noda_2.data)
+                merge_sublists(noda_1, noda_2.next, res_lst)
     
-    result = DoublyLinkedList()
-    merge_sublists(srt_lnk_lst1.header.next, srt_lnk_lst2.header.next, result)
-    return result
+    res = DoublyLinkedList()
+    merge_sublists(dll1.header.next, dll2.header.next, res)
+    return res
