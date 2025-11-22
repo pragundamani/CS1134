eaa72ef31178df352cab15b2ee2563bf0085f40f
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-21 21:46:36 -0500

    Update pd2752_hw6_q5.py

--- labs hw/hw6/pd2752_hw6_q5.py
+++ labs hw/hw6/pd2752_hw6_q5.py
@@ -1,1 +1,25 @@
 #q5 answer pd2752
+from DoublyLinkedList import DoublyLinkedList
+
+def merge_linked_lists(srt_lnk_lst1, srt_lnk_lst2):
+    def merge_sublists(node1, node2, result_list):
+        if node1 is srt_lnk_lst1.trailer and node2 is srt_lnk_lst2.trailer:
+            return
+        
+        if node1 is srt_lnk_lst1.trailer:
+            result_list.add_last(node2.data)
+            merge_sublists(node1, node2.next, result_list)
+        elif node2 is srt_lnk_lst2.trailer:
+            result_list.add_last(node1.data)
+            merge_sublists(node1.next, node2, result_list)
+        else:
+            if node1.data <= node2.data:
+                result_list.add_last(node1.data)
+                merge_sublists(node1.next, node2, result_list)
+            else:
+                result_list.add_last(node2.data)
+                merge_sublists(node1, node2.next, result_list)
+    
+    result = DoublyLinkedList()
+    merge_sublists(srt_lnk_lst1.header.next, srt_lnk_lst2.header.next, result)
+    return result
