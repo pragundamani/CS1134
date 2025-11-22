a9d625332b4e314c3292e19cdca197a32c4d5279
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-21 22:49:38 -0500

    Update pd2752_hw6_q4.py

--- labs hw/hw6/pd2752_hw6_q4.py
+++ labs hw/hw6/pd2752_hw6_q4.py
@@ -1,21 +1,18 @@
-#q4 answer pd2752
 from DoublyLinkedList import DoublyLinkedList
+import copy
 
 def copy_linked_list(dll):
     res = DoublyLinkedList()
     curr = dll.header.next
     while curr is not dll.trailer:
-        res.add_last(curr.data)
+        res.add_last(copy.copy(curr.data))
         curr = curr.next
     return res
-    
+
 def deep_copy_linked_list(dll):
     res = DoublyLinkedList()
     curr = dll.header.next
     while curr is not dll.trailer:
-        if isinstance(curr.data,DoublyLinkedList):
-            res.add_last(deep_copy_linked_list(curr.data))
-        else:
-            res.add_last(curr.data)
+        res.add_last(copy.deepcopy(curr.data))
         curr = curr.next
     return res
