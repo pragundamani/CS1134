ba834e9d3b217f918ff19255d66fe499f654af4b
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-30 00:33:39 +0000

    Implement subtree_min_and_max function and add new file for question 5

--- labs hw/hw7/pd2752_hw7_q1.py
+++ labs hw/hw7/pd2752_hw7_q1.py
@@ -1,1 +1,19 @@
 #q1 answer pd2752
+def subtree_min_and_max(bin_tree):
+    if bin_tree.is_empty():
+        return None, None
+    
+    minmax = [float('inf'), float('-inf')]
+    
+    def recursive_helper(root):
+        if root is None:
+            return None,None
+        if root.data < minmax[0]:
+            minmax[0] = root.data
+        if root.data > minmax[1]:
+            minmax[1] = root.data
+        recursive_helper(root.left)
+        recursive_helper(root.right)
+        return min,max
+    return recursive_helper(bin_tree.root)
+
