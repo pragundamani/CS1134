3510ca1204aeb731208fc288175975608d6391ad
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-11-30 00:08:44 +0000

    stuff

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def find(root,key):
     if root is None:
         return ['fail']
@@ -13,3 +14,31 @@ def find(root,key):
         return ['l']
     return      
         
+=======
+# empty list
+# list of 1
+# reallly long list of 1
+
+def thing(lst):
+    ind = 0 
+    while ind < len(lst)-1:
+        if lst[ind] == lst[ind+1]:
+            lst[ind+1], lst[len(lst)-1] = lst[len(lst)-1], lst[ind]
+            lst.pop()
+        else:
+            ind += 1
+            
+lst1 = []
+lst2 = [1]
+lst3 = [1]*10
+thing(lst1)
+thing(lst2)
+thing(lst3)
+print(lst1)
+print(lst2)
+print(lst3)
+
+lst_midterm = [2,2,2,3,3,1,1,1,2,2]
+thing(lst_midterm)
+print(lst_midterm)
+>>>>>>> 1a3a7fc (stuff)
