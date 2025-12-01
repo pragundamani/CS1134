cdf0e5023a91a5ce8b0318b4d59c14f044739847
Pragun Damani <146848887+pragundamani@users.noreply.github.com>
2025-12-01 03:43:13 +0000

    hw7

--- labs hw/hw7/pd2752_hw7_q2.py
+++ labs hw/hw7/pd2752_hw7_q2.py
@@ -1,1 +1,13 @@
 #q2 answer pd2752
+def leaves_list(self):
+    x = 0
+    def helper(node,x):
+        if node is None:
+            return 0
+        if node.left is None and node.right is None:
+            x = node.value
+            yield x
+        else:
+            yield from helper(node.left,x)
+            yield from helper(node.right,x)
+    return list(helper(self.root,x))
